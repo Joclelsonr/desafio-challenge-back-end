f5004110ab3dea669e55287598c4d62a60fe7ab6
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-12-05 16:05:00 -0300

    feat: :sparkles: Criação de documentação para os endpoint de pacientes

--- src/routes/patients.route.ts
+++ src/routes/patients.route.ts
@@ -1,9 +1,27 @@
 import { FastifyInstance } from "fastify";
 import { makePatientsFactory } from "../factories/patients.factory";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import {
+  createPatientSchema,
+  findPatientSchema,
+} from "../schemas/patients.schema";
 
 const patientsController = makePatientsFactory();
 
 export async function patientsRoutes(app: FastifyInstance) {
-  app.post("/patients", patientsController.create);
-  app.get("/patients/:patientId", patientsController.findOne);
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      "/patients",
+      { schema: createPatientSchema },
+      patientsController.create,
+    );
+
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get(
+      "/patients/:patientId",
+      { schema: findPatientSchema },
+      patientsController.findOne,
+    );
 }
--- src/schemas/patients.schema.ts
+++ src/schemas/patients.schema.ts
@@ -1,13 +1,93 @@
 import z from "zod";
 
-export const createPatientSchema = z.object({
-  name: z.string().min(3),
+import { FastifySchema } from "fastify";
+
+export const createPatientBody = z.object({
+  name: z.string().min(3).default("John Doe"),
   email: z.email(),
-  phone: z.string().min(14).max(15),
+  phone: z
+    .string()
+    .regex(
+      /^\(\d{2}\) \d{5}-\d{4}$/,
+      "Invalid phone number. Expected format: (99) 99999-9999",
+    ),
 });
 
 export const paramsPatientSchema = z.object({
   patientId: z.uuid({ message: "Invalid patient ID" }),
 });
 
-export type PatientCreateInput = z.infer<typeof createPatientSchema>;
+export type PatientCreateBody = z.infer<typeof createPatientBody>;
+export type PatientParams = z.infer<typeof paramsPatientSchema>;
+
+export const createPatientSchema: FastifySchema = {
+  tags: ["Pacientes"],
+  summary: "Cadastrar novo paciente",
+  description:
+    "Este endpoint permite o cadastro de um novo paciente no sistema. Para realizar o registro, é necessário fornecer os dados básicos do paciente, incluindo nome, e-mail e telefone.",
+  body: createPatientBody,
+  response: {
+    201: z
+      .object({
+        id: z.string(),
+        name: z.string(),
+        email: z.string(),
+        phone: z.string(),
+      })
+      .describe("Created successfully"),
+    400: z
+      .object({
+        statusCode: z.number().default(400),
+        error: z.string().default("Validation"),
+        message: z.string().default("Validation error name or email or phone"),
+      })
+      .describe("Validation error"),
+    409: z
+      .object({
+        statusCode: z.number().default(409),
+        error: z.string().default("Conflict"),
+        message: z.string().default("Patient already exists"),
+      })
+      .describe("Conflict error"),
+    500: z
+      .object({
+        statusCode: z.number().default(500),
+        error: z.string().default("Server Error"),
+        message: z.string().default("Internal server error"),
+      })
+      .describe("Internal server error"),
+  },
+};
+
+export const findPatientSchema: FastifySchema = {
+  tags: ["Pacientes"],
+  summary: "Obter paciente por ID, incluindo seus agendamentos",
+  description:
+    "Este endpoint recupera as informações de um paciente específico a partir de seu identificador único (ID). Além dos dados básicos do paciente como nome, e-mail e telefone, a resposta também inclui a lista de agendamentos associados a ele.",
+  params: paramsPatientSchema,
+  response: {
+    200: z
+      .object({
+        id: z.uuid(),
+        name: z.string(),
+        email: z.string(),
+        phone: z.string(),
+        appointments: z.array(z.any()),
+      })
+      .describe("Patient found successfully"),
+    404: z
+      .object({
+        statusCode: z.number().default(404),
+        error: z.string().default("Not Found"),
+        message: z.string().default("Patient not found"),
+      })
+      .describe("Not found error"),
+    500: z
+      .object({
+        statusCode: z.number().default(500),
+        error: z.string().default("Server Error"),
+        message: z.string().default("Internal server error"),
+      })
+      .describe("Internal server error"),
+  },
+};
